293af229ded8496167c7ca71f5d844ea5cabfe13
novabyte <novabyte@novabyte.blog>
2024-01-14 21:13:01 -0500

    adding signup validations

--- src/pages/SignUpPage.vue
+++ src/pages/SignUpPage.vue
@@ -4,16 +4,46 @@
       <q-card class="col-7 q-px-xl q-py-lg q-mt-xl">
         <p class="text-h2">Sign Up</p>
         <q-form @submit="onSubmit" @reset="onReset">
-          <q-input v-model="userName" label="Username" type="text" />
-          <q-input v-model="email" label="Email" type="text" />
-          <q-input v-model="password" label="Password" type="text" />
           <q-input
-            v-model="passwordConfirmation"
-            label="Confirm Password"
+            v-model="userName"
+            label="Username"
             type="text"
+            :rules="[() => userName.length > 0 || 'Username is required!']"
+          />
+          <q-input
+            v-model="email"
+            label="Email"
+            type="text"
+            :rules="[
+              () => email.length > 0 || 'Email is required!',
+              () => isEmail(email) || 'Not a valid email!',
+            ]"
+          />
+          <q-input
+            v-model="password"
+            label="Password"
+            type="password"
+            :rules="[() => password.length > 0 || 'Password is required!']"
+          />
+          <q-input
+            :model-value="passwordConfirmation"
+            label="Confirm Password"
+            type="password"
+            @update:model-value="confirmPasswordMatch"
+            :rules="[
+              () =>
+                passwordConfirmation === password || 'Passwords do not match!',
+            ]"
           />
           <q-card-actions align="around" class="q-px-lg q-pt-lg">
-            <q-btn type="submit" label="Launch" color="secondary" />
+            <q-btn
+              type="submit"
+              label="Launch"
+              color="secondary"
+              :disable="
+                passwordConfirmation !== password || password.length === 0
+              "
+            />
             <q-btn
               type="reset"
               label="Abort"
@@ -31,6 +61,7 @@
 <script setup lang="ts">
 import { usePersonClient } from 'src/models/person';
 import { ref } from 'vue';
+import { isEmail } from 'validator';
 
 const userName = ref('');
 const email = ref('');
@@ -39,11 +70,16 @@ const passwordConfirmation = ref('');
 
 const pc = usePersonClient();
 
-const onSubmit = async () => {
-  // https://stackoverflow.com/questions/3391242/should-i-hash-the-password-before-sending-it-to-the-server-side
-  const foo = await pc.signUp(userName.value, email.value, password.value);
+const confirmPasswordMatch = (val: string | number | null) => {
+  if (val === null) return;
+  passwordConfirmation.value = val.toString();
+};
 
-  console.log('onSubmit data:', foo);
+const onSubmit = async () => {
+  console.log('is email:', isEmail(email.value));
+  // const foo = await pc.signUp(userName.value, email.value, password.value);
+
+  // console.log('onSubmit data:', foo);
 };
 
 const onReset = () => {
