e51a07c049bfcc48c77be43577428c1b30cbae2d
novabyte <novabyte@novabyte.blog>
2024-12-03 15:34:44 -0500

    fixing crash when navigating

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,66 @@
 <template>
-  <router-view />
+  <suspense>
+    <router-view />
+  </suspense>
 </template>
 
 <script setup lang="ts">
-import { jwtDecode } from 'jwt-decode';
-import { storeToRefs } from 'pinia';
-import { usePersonStore } from 'src/models/person';
-import { useNovaStore } from 'src/stores/nova.store';
-import { watch } from 'vue';
-import { useRouter } from 'vue-router';
-import { RouteNames } from 'src/router/routes';
+  import { jwtDecode } from 'jwt-decode';
+  import { storeToRefs } from 'pinia';
+  import { usePersonStore } from 'src/models/person';
+  import { useNovaStore } from 'src/stores/nova.store';
+  import { watch } from 'vue';
+  import { useRouter } from 'vue-router';
+  import { RouteNames } from 'src/router/routes';
+  import { useOnce } from './composables/once';
+  import { onMounted } from 'vue';
+  import { useLogger } from './composables/useLogger';
 
-const router = useRouter();
-const nova = useNovaStore();
-const { noPersonButToken, isAuthenticated, currentPerson } = storeToRefs(
-  useNovaStore()
-);
-const ps = usePersonStore();
+  const log = useLogger('App.vue');
 
-// check for token immediately to set noPersonButToken
-nova.checkForToken();
+  const router = useRouter();
+  const nova = useNovaStore();
+  const { noPersonButToken, isAuthenticated, currentPerson } = storeToRefs(
+    useNovaStore()
+  );
+  const ps = usePersonStore();
 
-// set to immediate to attempt to refresh whenever a person loads the app
-watch(
-  noPersonButToken,
-  async (val) => {
-    if (!val) {
-      return;
-    }
-    let token = nova.getToken();
+  if (process.env.CLIENT) {
+    // check for token immediately to set noPersonButToken
+    nova.checkForToken();
 
-    if (!token) {
-      token = (await nova.refresh('noPersonButToken watcher')) ?? undefined;
-      if (!token) throw Error('attempted refresh but unable to');
-      nova.setToken(token);
-    }
+    // set to immediate to attempt to refresh whenever a person loads the app
+    onMounted(() => {
+      useOnce(() => watch(
+        noPersonButToken,
+        async (val) => {
+          log.debug('running noPersonButToken watcher. val: ' + val)
+          let token = nova.getToken();
 
-    const claims = jwtDecode(token);
-    if (!claims.sub) throw new Error('Expected jwt subject.');
+          if (!token) {
+            token = (await nova.refresh('noPersonButToken watcher')) ?? undefined;
+            if (!token) throw Error('attempted refresh but unable to');
+            nova.setToken(token);
+          }
 
-    console.log('noPersonButToken watcher', val);
-    let cp = await ps.getPerson(claims.sub);
-    currentPerson.value = cp;
-    ps.addPerson(cp);
-    noPersonButToken.value = false;
-  },
-  { immediate: true }
-);
+          const claims = jwtDecode(token);
+          if (!claims.sub) throw new Error('Expected jwt subject.');
 
-router.beforeEach(async (to) => {
-  if (to.meta.requiresAuth && !isAuthenticated.value) {
-    router.push({ name: RouteNames.HOME });
+          let cp = await ps.getPerson(claims.sub);
+          currentPerson.value = cp;
+          ps.addPerson(cp);
+          noPersonButToken.value = false;
+        },
+        { immediate: true }
+      ));
+    });
+
+    router.beforeEach(async (to, _from, next) => {
+      if (to.meta.requiresAuth && !isAuthenticated.value) {
+        router.push({ name: RouteNames.LOGIN });
+      } else {
+        next();
+      }
+    });
   }
-});
 </script>
