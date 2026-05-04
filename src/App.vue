97e3d589dd5a15d0eb0cfaf978e664e6daa25db1
novabyte <novabyte@novabyte.blog>
2026-05-04 13:30:06 -0400

    adding test change to make sure code changes propogate to the server - enhancing gitignore section for env files

--- src/App.vue
+++ src/App.vue
@@ -3,62 +3,64 @@
 </template>
 
 <script setup lang="ts">
-import { jwtDecode } from 'jwt-decode';
-import { storeToRefs } from 'pinia';
-import { usePersonStore } from 'src/models/person';
-import { useNovaStore } from 'src/stores/nova.store';
-import { watch } from 'vue';
-import { useRouter } from 'vue-router';
-import { RouteNames } from 'src/router/routes';
-import { useOnce } from './composables/once';
-import { onMounted } from 'vue';
-import { useLogger } from './composables/useLogger';
+  import { jwtDecode } from 'jwt-decode';
+  import { storeToRefs } from 'pinia';
+  import { usePersonStore } from 'src/models/person';
+  import { useNovaStore } from 'src/stores/nova.store';
+  import { watch } from 'vue';
+  import { useRouter } from 'vue-router';
+  import { RouteNames } from 'src/router/routes';
+  import { useOnce } from './composables/once';
+  import { onMounted } from 'vue';
+  import { useLogger } from './composables/useLogger';
 
-const log = useLogger('App.vue');
+  const log = useLogger('App.vue');
 
-const router = useRouter();
-const nova = useNovaStore();
-const { ghostToken, isAuthenticated, currentPerson, currentToken } =
-  storeToRefs(useNovaStore());
-const ps = usePersonStore();
+  const router = useRouter();
+  const nova = useNovaStore();
+  const { ghostToken, isAuthenticated, currentPerson, currentToken } =
+    storeToRefs(useNovaStore());
+  const ps = usePersonStore();
 
-if (process.env.CLIENT) {
-  // set to immediate to attempt to refresh whenever a person loads the app
-  onMounted(() => {
-    useOnce(() =>
-      watch(ghostToken, async (val) => {
-        log.debug('running ghostToken watcher. val: ' + val);
+  if (process.env.CLIENT) {
+    // set to immediate to attempt to refresh whenever a person loads the app
+    onMounted(() => {
+      useOnce(() =>
+        watch(ghostToken, async (val) => {
+          log.debug('running ghostToken watcher. val: ' + val);
 
-        if (!val) {
-          return;
-        }
+          if (!val) {
+            return;
+          }
 
-        if (!currentToken.value) {
-          await nova.refresh();
-          if (!currentToken.value) throw Error('attempted refresh but failed');
-        }
+          if (!currentToken.value) {
+            await nova.refresh();
+            if (!currentToken.value) throw Error('attempted refresh but failed');
+          }
 
-        const claims = jwtDecode(nova.currentToken as string);
-        if (!claims.sub) throw new Error('Expected jwt subject.');
+          const claims = jwtDecode(nova.currentToken as string);
+          if (!claims.sub) throw new Error('Expected jwt subject.');
 
-        let cp = await ps.getPerson(claims.sub);
-        currentPerson.value = cp;
-        ps.addPerson(cp);
-        ghostToken.value = false;
-      })
-    );
+          let cp = await ps.getPerson(claims.sub);
+          currentPerson.value = cp;
+          ps.addPerson(cp);
+          ghostToken.value = false;
+        })
+      );
 
-    log.debug('checking for auth paradox');
-    // check for token immediately to set ghostToken
-    nova.checkForAuthParadox();
-  });
+      log.debug('checking for auth paradox');
+      // check for token immediately to set ghostToken
+      nova.checkForAuthParadox();
 
-  router.beforeEach(async (to, _from, next) => {
-    if (to.meta.requiresAuth && !isAuthenticated.value) {
-      router.push({ name: RouteNames.LOGIN });
-    } else {
-      next();
-    }
-  });
-}
+      log.info('hello from the land of CI/CD!!!')
+    });
+
+    router.beforeEach(async (to, _from, next) => {
+      if (to.meta.requiresAuth && !isAuthenticated.value) {
+        router.push({ name: RouteNames.LOGIN });
+      } else {
+        next();
+      }
+    });
+  }
 </script>
